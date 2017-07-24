ff4e32c41f20f89a2170f4de8c33b44ab01fe795
chi wai <gwaydust@gmail.com>
2017-07-24 08:17:00 +0800

    added IE driver

--- SelFormFiller/Filler.cs
+++ SelFormFiller/Filler.cs
@@ -24,7 +24,7 @@ namespace SelFormFiller
                 service.HideCommandPromptWindow = true;
                 service.SuppressInitialDiagnosticInformation = true;
                 //service.ConnectToRunningBrowser = true;
-                driver = new FirefoxDriver(service);
+                driver = new FirefoxDriver(service);                
             } else
             {
                 driver = drv;
